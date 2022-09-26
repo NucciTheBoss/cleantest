856bf6fbc1f95cc5ab0a622f16a7b94a0475adca
NucciTheBoss <jason.nucciarone@canonical.com>
2022-09-26 09:36:25 -0400

    Add env data store

---
+++ src/simpletest/provider/data/env_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Environment variable management for test providers."""
+
+from typing import Any, Dict
+
+
+class EnvDataStore:
+    def __init__(self) -> None:
+        self.__env = {}
+
+    def append(self, env_var: str, value: str) -> None:
+        if env_var not in self.__env.keys():
+            self.__env.update({env_var: []})
+        
+        self.__env[env_var].append(value)
+
+    def get(self, env_var: str) -> Any:
+        if isinstance(self.__env[env_var], list):
+            return ":".join(self.__env[env_var])
+
+    @property
+    def _env(self) -> Dict[str, Any]:
+        return self.__env
