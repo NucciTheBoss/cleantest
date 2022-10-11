3c654766c027f9607f3454444acca797903e8456
NucciTheBoss <jason.nucciarone@canonical.com>
2022-10-11 11:04:22 -0400

    Add beginnings of package classes (#2)

---
+++ src/cleantest/pkg/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from .charmlib import CharmLibManager
+from .pip import PipManager
---
+++ src/cleantest/pkg/pip.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Manager for installing pip packages inside remote processes."""
+
+
+class PipManager:
+    def __init__(self) -> None:
+        raise NotImplementedError("PipManager not availbe until cleantest-0.2.0")
