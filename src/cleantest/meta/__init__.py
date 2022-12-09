c44915a7167eecf5aae057dc74ce7a8d66187abd
NucciTheBoss <jason.nucciarone@canonical.com>
2022-12-09 14:04:19 -0500

    refactor: Export Result

--- src/cleantest/meta/__init__.py
+++ src/cleantest/meta/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2022 Jason C. Nucciarone, Canonical Ltd.
 # See LICENSE file for licensing details.
 
-from .base_handler import BaseEntrypoint, BaseHandler, BaseHandlerError
+from .base_handler import BaseEntrypoint, BaseHandler, BaseHandlerError, Result
 from .base_package import BasePackage, BasePackageError
 from .cleantest_info import CleantestInfo
 from .injectable import Injectable
