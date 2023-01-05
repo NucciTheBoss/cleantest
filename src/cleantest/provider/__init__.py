f6f52e29569aa4f9b0fb827d31bd6447dc957e70
NucciTheBoss <jason.nucciarone@canonical.com>
2023-01-04 22:01:10 -0500

    bugfix: Fix up imports for lxd provider

--- src/cleantest/provider/__init__.py
+++ src/cleantest/provider/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2022 Jason C. Nucciarone, Canonical Ltd.
+# Copyright 2023 Jason C. Nucciarone, Canonical Ltd.
 # See LICENSE file for licensing details.
 
 from .lxd.lxd import lxd
-from .lxd.lxd_config import InstancePost, InstanceSource
