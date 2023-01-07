518cccbbbca79cf3128db3d8da73b220d4777725
NucciTheBoss <jason.nucciarone@canonical.com>
2023-01-07 14:59:07 -0500

    enhance: Move hooks module to under control

---
+++ src/cleantest/control/hooks/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright 2023 Jason C. Nucciarone, Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from .startenv import StartEnvHook
+from .stopenv import StopEnvHook
