2c12f0c0df2477164d18edd0815f49dec7c0d9aa
NucciTheBoss <jason.nucciarone@canonical.com>
2023-01-11 00:05:40 -0500

    enhance: Switch injectable over to base64 encoding

--- src/cleantest/meta/injectable.py
+++ src/cleantest/meta/injectable.py
@@ -1,78 +1,66 @@
 #!/usr/bin/env python3
-# Copyright 2022 Jason C. Nucciarone, Canonical Ltd.
+# Copyright 2023 Jason C. Nucciarone
 # See LICENSE file for licensing details.
 
-"""Metaclass for objects that will be injected into environments."""
+"""Abstract base class for injectable objects."""
 
+import base64
 import hashlib
-import pathlib
 import pickle
-import tempfile
-import uuid
 from abc import ABC, abstractmethod
-from collections import namedtuple
+from typing import Dict
 
 
 class InjectionError(Exception):
     """Base error for classes that inherit from Injectable."""
-
-    ...
-
-
-# Namedtuple returned by the _dump() method that contains
-# the path to the dumped object and its verification hash for future loading.
-InjectableData = namedtuple("InjectableData", ["path", "hash"])
 
 
 class Injectable(ABC):
     """Abstract metaclass that provides core methods needed by all injectable objects."""
 
     @classmethod
-    def _load(cls, data: str, verification_hash: str) -> object:
-        """Alternative constructor to load previously created object.
+    def _load(cls, checksum: str, data: str) -> object:
+        """Alternative constructor to load previously initialized object.
 
         Args:
+            checksum (str): Checksum to verify authenticity of serialized object.
             data (str): Path to file containing serialized object.
-            verification_hash (str): Hash to verify authenticity of serialized object.
 
         Returns:
             (object): Deserialized, verified object.
         """
-        fin = pathlib.Path(data)
-        if type(data) == str and fin.exists() and fin.is_file():
-            if verification_hash != hashlib.sha224(fin.read_bytes()).hexdigest():
-                raise InjectionError("Hashes do not match. Will not load untrusted object.")
+        if type(data) != str:
+            raise InjectionError(f"Cannot load object {data}. {type(data)} != str")
 
-            cls_data = pickle.loads(fin.read_bytes())
-            posargs = [
-                value for key, value in cls_data.__dict__.items() if not key.startswith("_")
-            ]
-            hiddenargs = {
-                key: value for key, value in cls_data.__dict__.items() if key.startswith("_")
-            }
-            new_cls = cls(*posargs)
-            [setattr(new_cls, key, value) for key, value in hiddenargs.items()]
-            return new_cls
-        else:
-            raise InjectionError(
-                f"Cannot load object {data}. Cannot find pickle file or {type(data)} is not str."
-            )
+        _ = base64.b64decode(data)
+        if checksum != hashlib.sha224(_).hexdigest():
+            raise InjectionError("Hashes do not match. Will not load untrusted object.")
 
-    def _dump(self) -> InjectableData:
-        """Serialize object and generate SHA224 hash for verification.
+        _ = pickle.loads(_)
+        posargs = [value for key, value in _.__dict__.items() if not key.startswith("_")]
+        hiddenargs = {key: value for key, value in _.__dict__.items() if key.startswith("_")}
+        new_cls = cls(*posargs)
+        [setattr(new_cls, key, value) for key, value in hiddenargs.items()]
+        return new_cls
+
+    def _dump(self, **kwargs) -> Dict[str, str]:
+        """Prepare object for injection.
 
         Returns:
-            (InjectableData):
-                path (str): Path to file containing serialized object.
-                hash (str): Hash to verify authenticity of serialized object.
+            (Dict[str, str]):
+                checksum (str): Checksum to verify authenticity of serialized object.
+                data (str): Base64 encoded string containing serialized object.
+                injectable (str): Injectable to run inside remote environment.
         """
-        fout = pathlib.Path(tempfile.gettempdir()).joinpath(f"{uuid.uuid4()}.pkl")
-        data = pickle.dumps(self)
-        fout.write_bytes(data)
-        verification_hash = hashlib.sha224(data).hexdigest()
-        return InjectableData(str(fout), verification_hash)
+        pickle_data = pickle.dumps(self)
+        checksum = hashlib.sha224(pickle_data).hexdigest()
+        data = base64.b64encode(pickle_data).decode()
+        return {
+            "checksum": checksum,
+            "data": data,
+            "injectable": self._injectable({"checksum": checksum, "data": data}, **kwargs),
+        }
 
     @abstractmethod
-    def __injectable__(self) -> str:
-        """Generate script that will be injected into test environment provider."""
-        ...
+    def _injectable(self, data: Dict[str, str], **kwargs) -> str:
+        """Python script to run inside of test environment provider."""
