c58fac8c995a1c802c0d1608b52787253743997d
NucciTheBoss <jason.nucciarone@canonical.com>
2023-01-31 20:44:33 -0500

    refactor: Rename snap and apt utilities

--- src/cleantest/utils/__init__.py
+++ src/cleantest/utils/__init__.py
@@ -2,6 +2,9 @@
 # Copyright 2023 Jason C. Nucciarone
 # See LICENSE file for licensing details.
 
-from . import apt_handler as apt
-from . import snap_handler as snap
+"""Utilities for testlets and cleantest."""
+
+from . import apt
+from . import snap
+from . import systemd
 from .run import run
