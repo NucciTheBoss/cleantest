0542f5789281f06617d50f014d79c5117d003b76
NucciTheBoss <jason.nucciarone@canonical.com>
2023-04-20 23:15:27 -0400

    refactor: Enhance LXD harness
    Notes:
    * Heavily focused in on removing all the complex code to simplify the interface. Now, rather than having a bunch of entrypoint classes, the harness just leverages the LXD archon.

---
+++ src/cleantest/lxd/harness.py
@@ -0,0 +1,128 @@
+# Copyright 2023 Jason C. Nucciarone
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Harness for the LXD test environment provider."""
+
+import functools
+import os
+import re
+from typing import Callable, Dict, List, Optional, Union
+
+from cleantest.data import File
+from cleantest.meta import Harness, Result
+from cleantest.meta.utils import thread_count
+
+from .archon import LXDArchon
+
+_match_lxd = re.compile(r"^@lxd\.target\(([^)]+)\)|^@lxd\(([^)]+)\)|^@lxd\(?\)?")
+
+
+class Error(Exception):
+    """Raise when LXD harness encounters an error."""
+
+
+class lxd(Harness):  # noqa N801
+    """LXD test environment provider.
+
+    Args:
+        name: Name for test environment. Default: "test".
+        image: LXD image to use for test environment Default: "ubuntu-jammy-amd64".
+        preserve: Preserve test environment after test has completed. Default: True.
+        num_threads:
+            Number of threads to use when running
+            test environment instances in parallel. Default: 1.
+    """
+
+    def __init__(
+        self,
+        name: str = "test",
+        image: Union[str, List[str]] = "ubuntu-jammy-amd64",
+        preserve: bool = True,
+        num_threads: Optional[int] = None,
+    ) -> None:
+        self._name = name
+        self._images = [image] if type(image) == str else image
+        self._preserve = preserve
+        if type(num_threads) != int or num_threads < 1:
+            os.environ["CLEANTEST_NUM_THREADS"] = str(thread_count())
+        else:
+            os.environ["CLEANTEST_NUM_THREADS"] = str(num_threads)
+
+    def __call__(self, func: Callable) -> Callable:
+        """Callable for lxd decorator."""
+
+        @functools.wraps(func)
+        def wrapper() -> Dict[str, Result]:
+            """Create new test environment instances and execute testlet.
+
+            Notes:
+                Pre-existing instances will have new testlet uploaded, but
+                StartEnvHooks will not be executed.
+            """
+            archon = LXDArchon()
+            instances = []
+            for image in self._images:
+                instances.append(f"{self._name}-{image}")
+            testlet = self._make_testlet(func, remove=[_match_lxd])
+            for name, image in zip(instances, self._images):
+                if archon.exists(name):
+                    archon.push(name, testlet, "/root/testlet")
+                else:
+                    archon.deploy(
+                        name, image, resources=[File(testlet, "/root/testlet")]
+                    )
+            results = archon.execute(instances, "python3 /root/testlet")
+            if not self._preserve:
+                archon.destroy()
+
+            return results
+
+        return wrapper
+
+    @classmethod
+    def target(
+        cls,
+        *instances: str,
+        num_threads: Optional[int] = None,
+    ) -> Callable:
+        """Target specific LXD test environment instances by name.
+
+        Args:
+            *instances: Test environment instance name.
+            num_threads: Number of threads to use for running testlets.
+        """
+        if type(num_threads) != int or num_threads < 1:
+            os.environ["CLEANTEST_NUM_THREADS"] = str(thread_count())
+        else:
+            os.environ["CLEANTEST_NUM_THREADS"] = str(num_threads)
+
+        def decorator(func: Callable) -> Callable:
+            @functools.wraps(func)
+            def wrapper() -> Dict[str, Result]:
+                """Execute testlet inside pre-existing instances."""
+                archon = LXDArchon()
+                for instance in instances:
+                    if not archon.exists(instance):
+                        raise Error(
+                            f"Test environment instance {instance} does not exist"
+                        )
+                testlet = cls._make_testlet(func)
+                archon.push([*instances], testlet, "/root/testlet")
+                results = archon.execute([*instances], "python3 /root/testlet")
+
+                return results
+
+            return wrapper
+
+        return decorator
---
+++ src/cleantest/meta/base_harness.py
@@ -0,0 +1,47 @@
+# Copyright 2023 Jason C. Nucciarone
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Base class for test environment provider Harness's."""
+
+import inspect
+from io import StringIO
+from typing import Callable, List, Pattern
+
+
+class Harness:
+    """Base class for test environment provider harnesses."""
+
+    @staticmethod
+    def _make_testlet(func: Callable, remove: List[Pattern] = None) -> str:
+        """Make injectable testlet from defined function.
+
+        Args:
+            func: Testlet function.
+            remove: Compiled regex pattern to remove from testlet.
+        """
+        src_code = inspect.getsource(func)
+        if remove is not None:
+            for regex in remove:
+                src_code = regex.sub(src_code, "")
+
+        testlet = StringIO()
+        testlet.writelines(
+            [
+                "#!/usr/bin/env python3\n",
+                f"{src_code}\n",
+                f"{func.__name__}()\n",
+            ]
+        )
+        testlet.seek(0)
+        return testlet.read()
